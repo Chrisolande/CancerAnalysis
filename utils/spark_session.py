a155b778ed428799d61d4f6801017ad412e0ca85
RealElvince <obuongely@yahoo.com>
2025-08-08 22:18:55 +0300

    code refactor for readability

--- utils/spark_session.py
+++ utils/spark_session.py
@@ -1,7 +1,8 @@
 from pyspark.sql import SparkSession
 
 def create_spark_session(app_name="Cancer Analysis"):
-   spark = ( SparkSession.builder\
+   spark = ( 
+     SparkSession.builder\
     .appName(app_name)\
     .config("spark.sql.shuffle.partitions","8")\
     .getOrCreate()
